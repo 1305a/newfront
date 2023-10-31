140b988bee91284d329a0197c694cf2d6519a857
Andrey <1305andrew2@gmail.com>
2023-10-31 16:49:56 +0300

    add new contents

--- assets/ContentAcceptCheckbox-122205a8.js
+++
@@ -1,1 +0,0 @@
-import{o as n,c}from"./index-b3cc15ff.js";const o=["innerHTML"],p={__name:"ContentAcceptCheckbox",props:["componentData"],setup(e){const t=e;return(a,s)=>(n(),c("div",{innerHTML:t.componentData.text,class:"content-accept"},null,8,o))}};export{p as default};
--- assets/ContentSubtitle-c87835cd.js
+++
@@ -1,1 +0,0 @@
-import{o as n,c as o}from"./index-b3cc15ff.js";const s=["innerHTML"],l={__name:"ContentSubtitle",props:["componentData"],setup(t){const e=t;return(c,_)=>(n(),o("div",{innerHTML:e.componentData.subtitle,class:"content-subtitle"},null,8,s))}};export{l as default};
